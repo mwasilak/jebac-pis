4046a8ed9a5e0cfc17e8d3805a2f8e8b4add8b49
Maciej Wasilak <wasilak@gmail.com>
2017-11-05 20:47:32 +0100

    Add /stages/add and stages/ routes to StageController

--- src/main/java/pl/sixpinetrees/tournament/web/StageController.java
+++ src/main/java/pl/sixpinetrees/tournament/web/StageController.java
@@ -1,7 +1,16 @@
 package pl.sixpinetrees.tournament.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import pl.sixpinetrees.tournament.domain.dto.StageForm;
+import pl.sixpinetrees.tournament.repository.StageRepository;
+import pl.sixpinetrees.tournament.service.StageService;
+
+import javax.validation.Valid;
 
 /**
  * Created by maciej on 23.03.17.
@@ -9,4 +18,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/stages")
 public class StageController {
+
+    @Autowired
+    private StageService stageService;
+
+    @GetMapping("/{stageId}")
+    public String stage(@PathVariable("stageId") Long stageId, Model model) {
+
+        model.addAttribute("stage", stageService.getStage(stageId));
+        return "stage";
+    }
+
+    @GetMapping("/add")
+    public String stageForm(StageForm stageForm) {
+        return "stageForm";
+    }
+
+    @PostMapping("/add")
+    public String processAddStage(@Valid StageForm stageForm, BindingResult bindingResult) {
+
+        if(bindingResult.hasErrors()) {
+            return "stageForm";
+        }
+
+        Long id = stageService.createStage(stageForm);
+        return "redirect:/stages/" + id;
+    }
+
 }
