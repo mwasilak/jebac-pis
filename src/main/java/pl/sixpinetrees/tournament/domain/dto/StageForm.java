fba564a0abd3e49eda119bc4858b14283922323d
Maciej Wasilak <wasilak@gmail.com>
2017-11-11 12:27:00 +0100

    Add name field on create stage form

--- src/main/java/pl/sixpinetrees/tournament/domain/dto/StageForm.java
+++ src/main/java/pl/sixpinetrees/tournament/domain/dto/StageForm.java
@@ -2,9 +2,19 @@ package pl.sixpinetrees.tournament.domain.dto;
 
 public class StageForm {
 
+    private String name;
+
     private Integer numberOfPlayers;
 
     public StageForm() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public Integer getNumberOfPlayers() {
