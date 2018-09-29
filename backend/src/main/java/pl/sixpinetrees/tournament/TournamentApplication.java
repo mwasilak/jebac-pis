b6e8a4cb6aa2d1ecbab6948e884087e574113b60
Maciej Wasilak <wasilak@gmail.com>
2018-09-29 23:07:53 +0200

    Add dummy tournament data

--- backend/src/main/java/pl/sixpinetrees/tournament/TournamentApplication.java
+++ backend/src/main/java/pl/sixpinetrees/tournament/TournamentApplication.java
@@ -5,14 +5,18 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Component;
+import pl.sixpinetrees.tournament.domain.Competition;
 import pl.sixpinetrees.tournament.domain.Match;
 import pl.sixpinetrees.tournament.domain.Round;
 import pl.sixpinetrees.tournament.domain.Player;
+import pl.sixpinetrees.tournament.domain.dto.CompetitionForm;
+import pl.sixpinetrees.tournament.repository.CompetitionRepository;
 import pl.sixpinetrees.tournament.repository.MatchRepository;
 import pl.sixpinetrees.tournament.repository.PlayerRepository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class TournamentApplication {
@@ -44,6 +48,14 @@ class DummyDataCLR implements CommandLineRunner {
         playerRepository.save(new Player("Joseph", "North"));
         playerRepository.save(new Player("Matt", "East"));
         playerRepository.save(new Player("Jack", "South"));
+
+        List<Player> playerList = new ArrayList<>();
+        playerList.addAll(playerRepository.findByFirstNameAndLastName("Johnny", "Bravo"));
+        playerList.addAll(playerRepository.findByFirstNameAndLastName("Marty", "McFly"));
+        playerList.addAll(playerRepository.findByFirstNameAndLastName("Barney", "Mead"));
+        playerList.addAll(playerRepository.findByFirstNameAndLastName("Lucy", "Prince"));
+
+        competitionRepository.save(new Competition("Competition",  playerList));
     }
 
     @Autowired
@@ -52,5 +64,7 @@ class DummyDataCLR implements CommandLineRunner {
     @Autowired
     private MatchRepository matchRepository;
 
+    @Autowired
+    private CompetitionRepository competitionRepository;
 }
 
