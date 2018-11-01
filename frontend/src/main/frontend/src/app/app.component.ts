cdff54dcd1a1f3f63037dcdcad77383c2a08563c
Maciej Wasilak <wasilak@gmail.com>
2018-11-01 20:38:06 +0100

    Add Material Design, remove datagrid component and Bootstrap CSS

--- frontend/src/main/frontend/src/app/app.component.ts
+++ frontend/src/main/frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MatIconRegistry} from "@angular/material";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tournament';
+  public constructor (public matIconRegistry: MatIconRegistry) {
+    //add custom material icons
+    matIconRegistry.registerFontClassAlias ('fontawesome','fa');
+  }
 }
