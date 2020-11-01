bca8f87cc8b772e09d9d18bb8d51f0cedf543867
Maciej Wasilak <maciej.wasilak@cgm.com>
2020-11-01 17:03:30 +0100

    Simplify project structure based on new Dave Syer example

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -3,7 +3,7 @@
 
 module.exports = function (config) {
   config.set({
-    basePath: '',
+    basePath: '../frontend/src/main/frontend/src',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
